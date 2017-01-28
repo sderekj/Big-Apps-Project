374ed9abf699e0ac3db5aac56be8b47be222237b
Shannon Alexander-Navarro <shannonalexander@hotmail.com>
2017-01-28 14:33:29 -0500

    initial commit

---
+++ app/src/main/java/nyc/c4q/dereksantos/bigappsproject/ResourcesFragment.java
@@ -0,0 +1,10 @@
+package nyc.c4q.dereksantos.bigappsproject;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by shannonalexander-navarro on 1/28/17.
+ */
+
+public class ResourcesFragment extends Fragment {
+}
---
+++ app/src/main/java/nyc/c4q/dereksantos/bigappsproject/ViewPageAdapter.java
@@ -0,0 +1,8 @@
+package nyc.c4q.dereksantos.bigappsproject;
+
+/**
+ * Created by shannonalexander-navarro on 1/28/17.
+ */
+
+public class ViewPageAdapter {
+}
