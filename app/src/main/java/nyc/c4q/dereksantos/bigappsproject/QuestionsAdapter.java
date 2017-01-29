ca623bb2cf352d080819c83579209743470cf869
sderekj <dereksantos1989@gmail.com>
2017-01-29 17:40:03 -0500

    Implemented Questions RecyclerView

---
+++ app/src/main/java/nyc/c4q/dereksantos/bigappsproject/QuestionsAdapter.java
@@ -0,0 +1,46 @@
+package nyc.c4q.dereksantos.bigappsproject;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by SACC on 1/29/17.
+ */
+public class QuestionsAdapter extends RecyclerView.Adapter<QuestionViewHolder> {
+    List<Question> questionList = Arrays.asList(
+            new Question("How are you?", "How the hell are you today?"),
+            new Question("How are you?", "How the hell are you today?"),
+            new Question("How are you?", "How the hell are you today?"),
+            new Question("How are you?", "How the hell are you today?"),
+            new Question("How are you?", "How the hell are you today?"),
+            new Question("How are you?", "How the hell are you today?"),
+            new Question("How are you?", "How the hell are you today?"),
+            new Question("How are you?", "How the hell are you today?"),
+            new Question("How are you?", "How the hell are you today?"),
+            new Question("How are you?", "How the hell are you today?"),
+            new Question("How are you?", "How the hell are you today?"),
+            new Question("How are you?", "How the hell are you today?"),
+            new Question("How are you?", "How the hell are you today?")
+    );
+
+
+    @Override
+    public QuestionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new QuestionViewHolder(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(QuestionViewHolder holder, int position) {
+        Question question = questionList.get(position);
+        holder.bind(question);
+    }
+
+    @Override
+    public int getItemCount() {
+        return questionList.size();
+    }
+}
